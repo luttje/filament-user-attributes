95b26e87623e8e52c0967a53ff0d20f50635638c
luttje <2738114+luttje@users.noreply.github.com>
2023-11-04 17:08:53 +0100

    fix host name

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -55,7 +55,7 @@ class TestCase extends Orchestra
             'driver' => env('DB_DRIVER', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', '3306'),
+            'database' => env('DB_DATABASE', 'test_filament_user_attributes'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
         ]);
